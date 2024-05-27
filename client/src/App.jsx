898db5d0ed722bf2dc19e4d5d4545f525254bec8
TimurGar <timur141633@gmail.com>
2024-05-27 16:27:44 -0400

    installed react js and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+    <h1 className='text-black'>App</h1>
+    <h2 className='text-black'>App</h2>
+    <h3 className='text-black'>App</h3>
+    </div>
+  )
+}
