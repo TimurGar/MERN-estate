51673d09b6fdb97403844c12deecde66ddcf99b2
TimurGar <timur141633@gmail.com>
2024-06-11 20:11:54 -0400

    delete user functionallity added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,9 @@ import {
   updateUserStart,
   updateUserFailure,
   updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
 
@@ -86,77 +89,101 @@ export default function Profile() {
     }
   };
 
-  return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          onChange={(e) => setFile(e.target.files[0])}
-          type="file"
-          ref={fileRef}
-          hidden
-          accept="image/*"
-        />
-        <img
-          onClick={() => fileRef.current.click()}
-          src={formData.avatar || currentUser.avatar}
-          alt="profile"
-          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-        />
-        <p className="text-sm self-center">
-          {fileUploadError ? (
-            <span className="text-red-700">
-              Error image upload (image must be less than 2 mb)
-            </span>
-          ) : filePerc > 0 && filePerc < 100 ? (
-            <span className="text-slate-700">Uploading ${filePerc}%</span>
-          ) : filePerc === 100 ? (
-            <span className="text-green-700">Image successfully uploaded</span>
-          ) : (
-            ""
-          )}
-        </p>
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      // getting the response from the server
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
 
-        <input
-          type="text"
-          placeholder="username"
-          defaultValue={currentUser.username}
-          className="border p-3 rounded-lg"
-          onChange={handleChange}
-          id="username"
-        />
-        <input
-          type="text"
-          placeholder="email"
-          defaultValue={currentUser.email}
-          className="border p-3 rounded-lg"
-          onChange={handleChange}
-          id="email"
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={handleChange}
-          id="password"
-          className="border p-3 rounded-lg"
-        />
-        <button
-          disabled={loading}
-          className="bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80"
-        >
-          {loading ? "Loading..." : "Update"}
-        </button>
-      </form>
-      <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">
-          Delete <a href=""></a>ccount
-        </span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+    };
+    return (
+      <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            onChange={(e) => setFile(e.target.files[0])}
+            type="file"
+            ref={fileRef}
+            hidden
+            accept="image/*"
+          />
+          <img
+            onClick={() => fileRef.current.click()}
+            src={formData.avatar || currentUser.avatar}
+            alt="profile"
+            className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          />
+          <p className="text-sm self-center">
+            {fileUploadError ? (
+              <span className="text-red-700">
+                Error image upload (image must be less than 2 mb)
+              </span>
+            ) : filePerc > 0 && filePerc < 100 ? (
+              <span className="text-slate-700">Uploading ${filePerc}%</span>
+            ) : filePerc === 100 ? (
+              <span className="text-green-700">
+                Image successfully uploaded
+              </span>
+            ) : (
+              ""
+            )}
+          </p>
+
+          <input
+            type="text"
+            placeholder="username"
+            defaultValue={currentUser.username}
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+            id="username"
+          />
+          <input
+            type="text"
+            placeholder="email"
+            defaultValue={currentUser.email}
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+            id="email"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={handleChange}
+            id="password"
+            className="border p-3 rounded-lg"
+          />
+          <button
+            disabled={loading}
+            className="bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Loading..." : "Update"}
+          </button>
+        </form>
+        <div className="flex justify-between mt-5">
+          <span
+            onClick={handleDeleteUser}
+            className="text-red-700 cursor-pointer"
+          >
+            Delete account
+          </span>
+          <span className="text-red-700 cursor-pointer">Sign out</span>
+        </div>
+        <p className="text-red-700 mt-5">{error ? error : ""}</p>
+        <p className="text-green-700 mt-5">
+          {updateSuccess ? "User is updated sucessfully" : ""}
+        </p>
       </div>
-      <p className="text-red-700 mt-5">{error ? error : ""}</p>
-      <p className="text-green-700 mt-5">
-        {updateSuccess ? "User is updated sucessfully" : ""}
-      </p>
-    </div>
-  );
-}
+    );
+  }
+
