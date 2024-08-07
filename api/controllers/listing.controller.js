1746542432bcf68751a787c7dc9786a45ee75d74
TimurGar <timur141633@gmail.com>
2024-08-07 00:54:50 -0400

    onChange and onSubmit functionallity on Search page added

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,7 +10,7 @@ export const createListing = async (req, res, next) => {
 };
 
 // Searching with MongoDB built-in search
-export const getListing = async (req, res, next) => {
+export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createListing, getListing } from "../controllers/listing.controller.js";
+import { createListing, getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 
-router.get("/get", getListing);
+router.get("/get", getListings);
 
 export default router;
